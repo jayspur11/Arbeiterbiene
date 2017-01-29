1a690026728837cf5546c6222f258c0ca1ef4b8d
Jaysen Spurlock <jayspur11@gmail.com>
2017-01-29 14:20:29 -0800

    Documented command decorator.

--- modules/module.py
+++ modules/module.py
@@ -4,6 +4,27 @@ bot = None
 module_commands = {}
 
 def module_command(func):
+    '''
+    Decorator to add a function as a command in the module.
+    
+    func (function):
+        The function to be registered in the module. It must take 1 argument of
+        type `discord.Message`.
+        **Note:** `message.content` will have the command stripped from
+        the front, leaving only the relevant pieces of the command in the
+        string.
+        
+        The function name will be used to index the function. For example,
+        decorating `def roll` will register the function under the `roll`
+        command in the module.
+    
+    Usage:
+        ```
+        @module_command
+        def <function_name>(message):
+            ...
+        ```
+    '''
     module_commands[func.__name__] = func
 
 async def process_message(message):
