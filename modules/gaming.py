c8bbb1f3081f44e4c4b74d431812806bf346c740
Jaysen Spurlock <jayspur11@gmail.com>
2017-01-29 14:03:46 -0800

    Spruced up Scion command.

--- modules/gaming.py
+++ modules/gaming.py
@@ -14,29 +14,51 @@ async def roll(message):
         results[i] = str(item)
     await bot.send_message(message.channel, ' + '.join(results))
 
+@module_command
+async def scion(message):
+    num_dice = 0
+    successes = 0
+    for arg in message.content.split(' '):
+        if arg[0] == 's':
+            successes += int(arg[1:])
+        elif arg[-1] == 's':
+            successes += int(arg[:-1])
+        elif arg[0] == 'e':
+            successes += scion_epic_successes(int(arg[1:]))
+        elif arg[-1] == 'e':
+            successes += scion_epic_successes(int(arg[:-1]))
+        else:
+            num_dice += int(arg)
+    results = []
+    for i in range(num_dice):
+        result = random.randint(1, 10)
+        if result > 6:
+            successes += 1
+        if result == 10:
+            successes += 1
+        results.append(result)
+    results.sort()
+    await bot.send_message(message.channel,
+                           scion_result_message(successes, results))
+
+## Helper Functions
+def scion_epic_successes(epic_attr_value):
+    if epic_attr_value == 0:
+        return 0
+    epic_successes = 1
+    for i in range(1, epic_attr_value):
+        epic_successes += i
+    return epic_successes
+
 def scion_result_message(successes, results):
-    firstResult = results[0]
+    firstResult = results[0] if len(results) else 0
     for i, item in enumerate(results):
         results[i] = str(item)
     message = ' + '.join(results)
-    if successes == 0 and firstResult == 1:
+    if firstResult == 0:
+        message = 'Got ' + str(successes) + ' successes without even trying.'
+    elif successes == 0 and firstResult == 1:
         message = 'A botch! ' + message
     else:
         message = str(successes) + ' successes! ' + message
     return message
-
-@module_command
-async def scion(message):
-    split = message.content.split(' ', 1)
-    numDice = int(split[0])
-    successes = int(split[1])
-    if numDice > 0:
-        results = []
-        for i in range(numDice):
-            results.append(random.randint(1, 10))
-            if results[-1] > 6:
-                successes += 1
-            if results[-1] == 10:
-                successes += 1
-        results.sort()
-        await bot.send_message(message.channel, scion_result_message(successes, results))
