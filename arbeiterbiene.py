0d9e736151901996998e48b7f453019f08a87098
Jaysen Spurlock <jayspur11@gmail.com>
2017-01-29 17:06:20 -0800

    Main file documentation.

--- arbeiterbiene.py
+++ arbeiterbiene.py
@@ -1,19 +1,27 @@
+'''
+# arbeiterbiene.py
+
+This is the main file for the Discord bot. It's responsible for the primary
+setup of the bot, as well as pulling in and configuring all the modules.
+'''
+
+from discord.ext import commands
 from modules import shared
 import asyncio
 import json
 import logging
 
 ## Module Setup
+'''
+How To Module:
+Import the module from `modules`. Then add it to the `module_registry` dict,
+with its trigger character as the key.
+'''
 from modules import gaming
 
 module_registry = {
     '$': gaming,
 }
-
-## Bot Setup
-from discord.ext import commands
-
-shared.bot = commands.Bot(('BOT! ', '!'))
 
 ## Bot Events
 @shared.bot.event
@@ -26,9 +34,11 @@ async def on_message(message):
     if trigger in module_registry:
         await module_registry[trigger].process_message(message)
     else:
+        # TODO: Remove this after the commands have been migrated.
         await shared.bot.process_commands(message)
 
 ## Bot Commands
+# TODO: Move these to a 'core' module.
 @shared.bot.command()
 async def die():
     raise KeyboardInterrupt
@@ -74,6 +84,7 @@ def main():
     '''
     The entry point of the Arbeiterbiene bot.
     '''
+    shared.bot = commands.Bot(('BOT! ', '!'))
     _configureFileLogging()
     try:
         with open('auth.json', 'r') as authfile:
