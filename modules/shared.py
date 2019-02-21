673dca827520f073635d967bec6d0aaf3bcee19c
Jaysen Spurlock <jayspur11@gmail.com>
2019-02-21 09:17:17 -0800

    Add Shared function to parse durations
    This allows us to extract a number of seconds from a string like '1d2h3m'.

--- modules/shared.py
+++ modules/shared.py
@@ -1,1 +1,27 @@
 bot = None
+
+
+def parse_duration(duration):
+    """ Parses a duration specifier ([#d][#h][#m]) into seconds.
+
+    :param duration: (string) String to extract time length from.
+    :return: (int) Number of seconds, or None if duration was not valid.
+    """
+    duration_seconds = 0
+    possible_durations = [
+        ('d', 86400),
+        ('h', 3600),
+        ('m', 60)
+    ]
+
+    for possible_duration, num_seconds in possible_durations:
+        if possible_duration in duration:
+            dur_split = duration.split(possible_duration, 1)
+            dur = dur_split[0]
+            duration = dur_split[1]
+            try:
+                duration_seconds += int(dur) * num_seconds
+            except ValueError:
+                return None
+
+    return duration_seconds if duration_seconds and not duration else None
