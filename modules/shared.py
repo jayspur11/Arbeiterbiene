d7e266c923b699a19914e3d84c6743e0b48157ce
Jaysen Spurlock <jayspur11@gmail.com>
2019-02-21 19:24:32 -0800

    Clean up the last commit.
    Renaming variables & adding some comments.

--- modules/shared.py
+++ modules/shared.py
@@ -1,27 +1,31 @@
 bot = None
 
 
-def parse_duration(duration):
+def parse_duration(sduration):
     """ Parses a duration specifier ([#d][#h][#m]) into seconds.
 
-    :param duration: (string) String to extract time length from.
-    :return: (int) Number of seconds, or None if duration was not valid.
+    :param sduration: (string) String to extract time length from.
+    :return: (int) Number of seconds, or None if sduration was not valid.
     """
     duration_seconds = 0
-    possible_durations = [
+    duration_values = [
         ('d', 86400),
         ('h', 3600),
         ('m', 60)
     ]
 
-    for possible_duration, num_seconds in possible_durations:
-        if possible_duration in duration:
-            dur_split = duration.split(possible_duration, 1)
-            dur = dur_split[0]
-            duration = dur_split[1]
+    for possible_duration, num_seconds in duration_values:
+        if possible_duration in sduration:
+            segment_duration, sduration = sduration.split(possible_duration, 1)
             try:
-                duration_seconds += int(dur) * num_seconds
+                duration_seconds += int(segment_duration) * num_seconds
             except ValueError:
                 return None
+    if sduration:
+        # trailing info. don't know what was actually meant.
+        return None
+    if not duration_seconds:
+        # 0 seconds? that's impossible. I won't allow it.
+        return None
 
-    return duration_seconds if duration_seconds and not duration else None
+    return duration_seconds
