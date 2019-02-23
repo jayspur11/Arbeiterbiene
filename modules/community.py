27c543330757d43f6dc21b86d515b1ac4e45e09d
Jaysen Spurlock <jayspur11@gmail.com>
2019-02-22 20:48:41 -0800

    Add poll answer extractors.
    Wrote some functions to help extract emoji from poll answers, to let us add the reactions to the poll message.

--- modules/community.py
+++ modules/community.py
@@ -14,14 +14,33 @@ from modules import shared
 def register_commands():
     @module.module_command
     async def poll(message):
-        """```poll <question> <answer 1 emote> <answer 2 emote> ... [time limit]```
+        """```poll "<question>" "<answer 1> <emoji>" ... "<answer N> <emoji>" [time limit]```
         Structures the poll and echoes it back to the server. After [time
         limit] has elapsed, counts the votes and notifies OP of the results.
         Default time limit is 24h.
-        Answers should end with an emote, so users can vote with a reaction.
+        Answers should end with an emoji, so users can vote with a reaction.
+        **The quotes are important!**
         """
         # parse answers
         # construct poll
         # echo to server
         # add reactions
         # TODO: eventually count the results
+
+
+def _extract_poll_emoji(sanswer, server):
+    """ Parses a poll answer (<string> <emoji>) for its emoji.
+
+    :param sanswer: (string) String containing a poll answer and corresponding
+        emoji.
+    :param server: (discord.Server) Server to check for custom Emoji.
+    :return: (?) Either a discord.Emoji object (for custom emoji), a string (for
+        unicode emoji), or None (for an invalid string).
+    """
+    # TODO: check string validity
+    emoji = sanswer.rsplit(' ', 1)[-1]
+    if ':' in emoji:
+        # emoji are formatted like '<:name:id>' and we want 'id'
+        emoji_id = emoji.split(':')[-1][:-1]
+        emoji = shared.get_emoji_by_id(emoji_id, server)
+    return emoji
