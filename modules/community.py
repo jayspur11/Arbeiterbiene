a6ed5a121df06d9f5140adc324df980c2b5bc28c
Jaysen Spurlock <jayspur11@gmail.com>
2019-07-07 18:32:57 -0700

    Drop formatting restriction on polls.
    It's silly to make people put quotes and all that. Let's just let them format the poll how they want it to go out, and echo it back. We can search the string for emoji and react appropriately.

--- modules/community.py
+++ modules/community.py
@@ -7,67 +7,39 @@ A command does *not* belong here if it...
  - is an introvert.
 """
 
+import emoji
+import re
 from datetime import datetime
 from modules import module
 from modules import shared
 
 
+_server_emoji_re = re.compile(r'<:.*:(.*)>')
+
+
 def register_commands():
     @module.module_command
     async def poll(message):
-        """```poll "<question>" "<answer 1> <emoji>" ... "<answer N> <emoji>" [time limit]```
-        Structures the poll and echoes it back to the server. After [time
-        limit] has elapsed, counts the votes and notifies OP of the results.
-        Default time limit is 24h.
-        Answers should end with an emoji, so users can vote with a reaction.
-        **The quotes are important!**
+        """```poll <question>
+        <answer 1> <emojus>
+        ...
+        <answer N> <emojus>```
+        Echoes the poll back to the server and adds the emoji as reactions.
         """
-        answer_list = []
-        poll_duration = 86400  # default, 24h
-        segments = message.content.split('" ')
-        for answer in segments[1:]:
-            if '"' not in answer:
-                poll_duration = shared.parse_duration(answer)
-                if not poll_duration:
-                    raise ValueError
-                break
-            answer_list.append(_parse_poll_answer(answer.strip('"'),
-                                                  message.server))
-
-        poll_content = "Hey @everyone! {op} would like to know:\n{q}".format(
-            op=message.author.mention, q=segments[0][1:])
-        for option, emoji in answer_list:
-            poll_content = "{prev}\n{emoji} for {option}".format(
-                prev=poll_content, emoji=emoji, option=option)
+        poll_content = "Hey everyone! {op} would like to know:\n{q}".format(
+            op=message.author.mention, q=message.content)
         poll_message = await shared.bot.send_message(message.channel,
                                                      poll_content)
 
-        for _, emoji in answer_list:
-            await shared.bot.add_reaction(poll_message, emoji)
+        server = message.server
+        for emojus_match in _server_emoji_re.finditer(message.content):
+            emojus = shared.get_emoji_by_id(emojus_match.group(1), server)
+            await shared.bot.add_reaction(poll_message, emojus)
+        for emojus_match in emoji.get_emoji_regexp().finditer(message.content):
+            await shared.bot.add_reaction(poll_message, emojus_match.group())
 
-        def tally_votes():
-            # TODO: count and notify
-            pass
-        poll_end_time = int(datetime.now().timestamp()) + poll_duration
-        shared.schedule_activity(poll_end_time, tally_votes)
-
-
-def _parse_poll_answer(sanswer, server):
-    """ Parses a poll answer (<string> <emoji>) for its emoji.
-
-    :param sanswer: (string) String containing a poll answer and corresponding
-        emoji.
-    :param server: (discord.Server) Server to check for custom Emoji.
-    :return: (tuple) A tuple containing a string respresenting the option, and
-        either a discord.Emoji object (for custom emoji) or a string (for
-        unicode emoji) representing the emoji reaction.
-
-    :raises ValueError if the answer string is invalid.
-    """
-    # TODO: check string validity
-    answer, emoji = sanswer.rsplit(' ', 1)
-    if ':' in emoji:
-        # emoji are formatted like '<:name:id>' and we want 'id'
-        emoji_id = emoji.split(':')[-1][:-1]
-        emoji = shared.get_emoji_by_id(emoji_id, server)
-    return answer, emoji
+        # def tally_votes():
+        #     # TODO: count and notify
+        #     pass
+        # poll_end_time = int(datetime.now().timestamp()) + poll_duration
+        # shared.schedule_activity(poll_end_time, tally_votes)
