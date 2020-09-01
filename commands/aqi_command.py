20c387f5729d114d27a67d99d196131cc1306ed4
Jaysen Spurlock <jayspur11@gmail.com>
2020-08-31 19:05:37 -0700

    Make AQI request & send the results

--- commands/aqi_command.py
+++ commands/aqi_command.py
@@ -1,4 +1,10 @@
+import json
+import re
 from commands.base_command import BaseCommand
+from urllib import request
+
+_zip_code_re = re.compile(r'\d{5}')
+
 
 class AqiCommand(BaseCommand):
     """Class to add 'aqi' command to bot."""
@@ -20,4 +26,26 @@ class AqiCommand(BaseCommand):
         """
 
     async def run(self, command_io):
-        pass
+        if not len(command_io.message.content):
+            raise ValueError
+        zip_code_match = _zip_code_re.search(command_io.message.content)
+        if not zip_code_match:
+            raise ValueError
+        req = request.Request(
+            "https://airnowapi.org/aq/observation/zipCode/current"
+            "?zipCode={zip_code}&format=application/json&api_key={api_key}"
+            .format(zip_code=zip_code_match.group(), api_key=self._api_key))
+        with request.urlopen(req) as raqi:
+            results = json.loads(raqi.read())
+            message_list = [
+                "Results for {zip}:".format(zip=zip_code_match.group())
+            ]
+            for result in results:
+                message_list.append(
+                    "AQI ({param}): {aqi} - {category}".format(
+                        param=result.get('ParameterName', '{error}'),
+                        aqi=result.get('AQI', '{error}'),
+                        category=result.get('Category', {}).get('Name', '{error}')
+                    )
+                )
+            await command_io.message.channel.send("\n".join(message_list))
