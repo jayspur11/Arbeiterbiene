e7cdbb616bf97f1570b019b6e1354a043242e48d
Jaysen Spurlock <jayspur11@gmail.com>
2020-09-08 21:03:56 -0700

    Refactor all database handling into the class.

--- shared/storage.py
+++ shared/storage.py
@@ -2,24 +2,20 @@ import asyncio
 import sqlite3
 
 
-async def _connect_to_db():
-    global _connection
-    _connection = sqlite3.connect('arbeiterbiene.db')
-
-
-# Create a dedicated thread for database use.
-_db_thread = asyncio.new_event_loop()
-_db_thread.run_until_complete(_connect_to_db())
-_cursor = _connection.cursor()
-
-
 class DatabaseHandler:
-    def __init__(self, table_name, column_names):
+    def __init__(self, table_name, column_names, db_name='arbeiterbiene.db'):
+        self._db_thread = asyncio.new_event_loop()
+        self._db_thread.run_until_complete(self._connect_to_db(db_name))
+        self._cursor = self._connection.cursor()
+
         self._table_name = table_name
-        _db_thread.run_until_complete(
-            self._execute_statement('CREATE TABLE IF NOT EXISTS ' +
-                                    table_name + ' (' +
-                                    ', '.join(column_names) + ');'))
+        self._db_thread.run_until_complete(
+            self._execute_statement(
+                'CREATE TABLE IF NOT EXISTS {table} ({cols});'.format(
+                    table=table_name, cols=', '.join(column_names))))
+
+    async def _connect_to_db(self, db_name):
+        self._connection = sqlite3.connect(db_name)
 
     async def _execute_statement(self, *args):
-        _cursor.execute(*args)
+        return self._cursor.execute(*args)
