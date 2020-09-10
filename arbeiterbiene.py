04e378114081f707ad33553811653dee779d3e4a
Jaysen Spurlock <jayspur11@gmail.com>
2020-09-10 16:27:29 -0700

    Move bot functionality as-is to a Client subclass.

--- arbeiterbiene.py
+++ arbeiterbiene.py
@@ -1,39 +1,38 @@
 """ This is the main file for the Discord bot. It's responsible for the primary
 setup of the bot, as well as pulling in and configuring all the modules.
 """
-import asyncio
 import commands
 import json
 import logging
 import re
-from discord.ext import commands as discord_commands
-
-_event_loop = asyncio.get_event_loop()
-_bot = discord_commands.Bot("", loop=_event_loop)
-_cmd_re = re.compile(r'(([^\s]+)\s*){2}(.*)')
+from discord.client import Client
 
 
-@_bot.event
-async def on_ready():
-    print("Hello world!")
+class Arbeiterbiene(Client):
+    _cmd_re = re.compile(r'(([^\s]+)\s*){2}(.*)')
 
+    def __init__(self, airnowapi_key):
+        super().__init__()
+        self._command_registry = commands.command_registry(airnowapi_key)
 
-@_bot.event
-async def on_message(message):
-    if not (type(message.channel) in ["DMChannel", "GroupChannel"]
-            or _bot.user.id in message.raw_mentions):
-        return
-    cmd, message.content = _cmd_re.match(message.content).group(2,3)
-    cmd = cmd.lower()
-    if cmd not in _command_registry:
-        # TODO: send an error message
-        return
-    command = _command_registry[cmd]
-    command_io = commands.CommandIO(_bot, _event_loop, message)
-    try:
-        await command.run(command_io)
-    except (IndexError, ValueError, KeyError):
-        await message.channel.send(command.help_text())
+    async def on_ready(self):
+        print("Hello world!")
+
+    async def on_message(self, message):
+        if not (type(message.channel) in ["DMChannel", "GroupChannel"]
+                or self.user.id in message.raw_mentions):
+            return
+        cmd, message.content = self._cmd_re.match(message.content).group(2, 3)
+        cmd = cmd.lower()
+        if cmd not in self._command_registry:
+            # TODO: send an error message
+            return
+        command = self._command_registry[cmd]
+        command_io = commands.CommandIO(message)
+        try:
+            await command.run(command_io)
+        except (IndexError, ValueError, KeyError):
+            await message.channel.send(command.help_text())
 
 
 def _configure_file_logging():
@@ -56,20 +55,18 @@ def main():
     """
     The entry point of the Arbeiterbiene bot.
     """
-    global _command_registry
     _configure_file_logging()
     try:
         with open('auth.json', 'r') as authfile:
             auth = json.load(authfile)
-            _command_registry = commands.command_registry(
-                auth.get('airnowapi_key', ''))
     except FileNotFoundError:
         print(
             'Please set up an "auth.json" file in the same directory as '
             '"arbeiterbiene.py". It should follow the format of "example-auth'
             '.json".')
         return
-    _bot.run(auth['token'])
+    bot = Arbeiterbiene(auth.get('airnowapi_key', ''))
+    bot.run(auth['token'])
 
 
 if __name__ == '__main__':
--- commands/command_io.py
+++ commands/command_io.py
@@ -1,10 +1,6 @@
-from asyncio.events import AbstractEventLoop
-from discord.ext.commands.bot import Bot
 from discord.message import Message
 
 
 class CommandIO:
-    def __init__(self, bot: Bot, event_loop: AbstractEventLoop, message: Message):
-        self.bot = bot
-        self.event_loop = event_loop
+    def __init__(self, message: Message):
         self.message = message
