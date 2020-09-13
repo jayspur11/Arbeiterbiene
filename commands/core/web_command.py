2d4e59512f300e3f48b3120f45be49b0f3032866
Jaysen Spurlock <jayspur11@gmail.com>
2020-09-13 11:12:01 -0700

    Drop 'async' from fetch method & decode responses.

--- commands/core/web_command.py
+++ commands/core/web_command.py
@@ -5,18 +5,21 @@ from urllib import request
 class WebCommandRequest:
     def __init__(self, identifier, url, headers={}):
         self.identifier = identifier
-        self.request = request.Request(
-            url, headers={'User-Agent': 'arbeiterbiene', **headers})
+        self.request = request.Request(url,
+                                       headers={
+                                           'User-Agent': 'arbeiterbiene',
+                                           **headers
+                                       })
 
 
 class WebCommand(base_command.BaseCommand):
     """Base class for commands that fetch information from the web."""
-    async def fetch_web_responses(self, command_io):
+    def fetch_web_responses(self, command_io):
         results = {}
-        async with command_io.message.channel.typing():
+        with command_io.message.channel.typing():
             for cmd_request in self.build_requests(command_io):
                 with request.urlopen(cmd_request.request) as req:
-                    results[cmd_request.identifier] = req.read()
+                    results[cmd_request.identifier] = req.read().decode()
         return results
 
     def build_requests(self, command_io):
