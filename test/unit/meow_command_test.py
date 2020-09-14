0b0ff95e199cf1b4c628b45632560448672e4a45
Jaysen Spurlock <jayspur11@gmail.com>
2020-09-13 19:32:10 -0700

    Update `meow` test to reflect actual requests

--- test/unit/meow_command_test.py
+++ test/unit/meow_command_test.py
@@ -8,13 +8,29 @@ from test.shared import async_mock
 from unittest import mock
 
 
+def urlopen_mocked(req):
+    mock_response = mock.Mock()
+    if "img" in req.full_url:
+        mock_response.read.return_value = b"""
+            {
+                "link": "linkurl"
+            }
+        """
+    elif "fact" in req.full_url:
+        mock_response.read.return_value = b"""
+            {
+                "fact": "funfact"
+            }
+        """
+
+    mock_context_manager = mock.MagicMock()
+    mock_context_manager.__enter__.return_value = mock_response
+    return mock_context_manager
+
+
 class MeowCommandTest(unittest.TestCase):
-    @mock.patch('json.loads')
-    @mock.patch('urllib.request.urlopen')
-    def test_basic_command(self, mock_urlopen, mock_loads):
-        mock_urlopen.__enter__ = mock.Mock(return_value=(mock.Mock(), None))
-        mock_urlopen.__exit__ = mock.Mock(return_value=None)
-        mock_loads.return_value = {'fact': 'funfact', 'link': 'linkurl'}
+    @mock.patch('urllib.request.urlopen', side_effect=urlopen_mocked)
+    def test_basic_command(self, mock_urlopen):
         mock_cmdio = async_mock.AsyncMock()
         command = commands.MeowCommand()
         asyncio.get_event_loop().run_until_complete(command.run(mock_cmdio))
