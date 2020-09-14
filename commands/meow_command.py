09a8061b5d558446ef0d57529712c03dc05f3e57
Jaysen Spurlock <jayspur11@gmail.com>
2020-09-13 19:38:06 -0700

    `meow` -> WebCommand

--- commands/meow_command.py
+++ commands/meow_command.py
@@ -1,11 +1,11 @@
 import json
 
-from commands.core import base_command
+from commands.core import web_command
 from discord import Embed
 from urllib import request
 
 
-class MeowCommand(base_command.BaseCommand):
+class MeowCommand(web_command.WebCommand):
     """Class to add a 'meow' command to the bot."""
     @classmethod
     def trigger_word(cls):
@@ -16,13 +16,17 @@ class MeowCommand(base_command.BaseCommand):
         Retrieves a random image of a cat, and a fun fact.
         """
 
+    def build_requests(self, command_io):
+        return [
+            web_command.WebCommandRequest(
+                "fact", "https://some-random-api.ml/facts/cat"),
+            web_command.WebCommandRequest(
+                "image", "https://some-random-api.ml/img/cat")
+        ]
+
     async def run(self, command_io):
-        freq = request.Request("https://some-random-api.ml/facts/cat",
-                               headers={"User-Agent": "arbeiterbiene"})
-        ireq = request.Request("http://some-random-api.ml/img/cat",
-                               headers={"User-Agent": "arbeiterbiene"})
-        with request.urlopen(freq) as rfact, request.urlopen(ireq) as rimg:
-            fact = json.loads(rfact.read())["fact"]
-            img_url = json.loads(rimg.read())["link"]
-            await command_io.message.channel.send(
-                fact, embed=Embed().set_image(url=img_url))
+        responses = self.fetch_web_responses(command_io)
+        await command_io.message.channel.send(
+            json.loads(responses["fact"])["fact"],
+            embed=Embed().set_image(url=json.loads(responses["image"])["link"])
+        )
