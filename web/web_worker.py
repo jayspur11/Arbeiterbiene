e0d39df9ee8ee8f9d7f675aa42cd7ad1a1bc0381
Jaysen Spurlock <jayspur11@gmail.com>
2020-09-17 11:25:48 -0700

    Create a class to retrieve & cache web data

---
+++ web/web_worker.py
@@ -0,0 +1,19 @@
+from urllib import request
+from web import web_hive
+
+class WebWorker:
+    """Class to fetch information from the web."""
+    cache = web_hive.WebHive()
+    
+    def make_request(self, url):
+        cached = self.cache.get(url)
+        if cached:
+            return cached
+        
+        web_request = request.Request(url, headers={
+            "User-Agent": "arbeiterbiene"
+        })
+        with request.urlopen(web_request) as response:
+            result = response.read().decode()
+        self.cache.store(url, result)
+        return result
