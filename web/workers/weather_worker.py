1524007cec34cdc5f9d11d17d2d34bd21fdb171c
Jaysen Spurlock <jayspur11@gmail.com>
2020-09-17 18:56:44 -0700

    Create worker for weather, which uses geocode.

---
+++ web/workers/weather_worker.py
@@ -0,0 +1,38 @@
+import json
+
+from web import web_worker
+from web.workers import geocode_worker
+
+
+class WeatherWorker(web_worker.WebWorker):
+    """Class to fetch information about the weather for a given location.
+    
+    Done using OpenWeatherMap.org.
+    """
+    def __init__(self, api_key):
+        """
+        Args:
+            api_key (string): API key to use for OWM requests.
+        """
+        self.ttl = 600
+        self._api_key = api_key
+
+    def fetch(self, zip_code):
+        """Retrieve weather information.
+
+        Args:
+            zip_code (string): Zip code to retrieve weather for.
+
+        Returns (tuple):
+            dict{string:string}: Current weather.
+            list[dict{string:string}]: Forecasted weather (one item per day,
+                starting with the current day).
+        """
+        geocode = geocode_worker.GeocodeWorker().fetch(zip_code)
+        url = ("https://api.openweathermap.org/data/2.5/onecall?"
+               "lat={lat}&lon={lon}&exclude=minutely,hourly&appid={key}&"
+               "units=imperial".format(lat=geocode["latitude"],
+                                       lon=geocode["longitude"],
+                                       key=self._api_key))
+        weather = json.loads(self._make_request(url))
+        return (weather["current"], weather["daily"])
