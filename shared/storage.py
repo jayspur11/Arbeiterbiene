c39f802d0864156e79bd875f1d585a1f69fc2d0a
Jaysen Spurlock <jayspur11@gmail.com>
2020-09-19 09:05:40 -0700

    Put database stuff on a dedicated thread.

--- shared/storage.py
+++ shared/storage.py
@@ -1,4 +1,25 @@
+import asyncio
 import sqlite3
 
-_connection = sqlite3.connect('arbeiterbiene.db')
+
+async def _connect_to_db():
+    global _connection
+    _connection = sqlite3.connect('arbeiterbiene.db')
+
+
+# Create a dedicated thread for database use.
+_db_thread = asyncio.new_event_loop()
+_db_thread.run_until_complete(_connect_to_db())
 _cursor = _connection.cursor()
+
+
+class DatabaseHandler:
+    def __init__(self, table_name, column_names):
+        self._table_name = table_name
+        _db_thread.run_until_complete(
+            self._execute_statement('CREATE TABLE IF NOT EXISTS ' +
+                                    table_name + ' (' +
+                                    ', '.join(column_names) + ');'))
+
+    async def _execute_statement(self, *args):
+        _cursor.execute(*args)
