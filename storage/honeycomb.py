6236a89d5569ff451374dc1058c1b8e02b07f315
Jaysen Spurlock <jayspur11@gmail.com>
2020-09-20 08:26:54 -0700

    Polish async

--- storage/honeycomb.py
+++ storage/honeycomb.py
@@ -2,29 +2,16 @@ import asyncio
 import sqlite3
 
 
-async def _connect():
-    """Async wrapper to connect to SQL DB on a dedicated thread."""
-    return sqlite3.connect("hive.db")
+async def _run_async(f, *args, **kwargs):
+    """Async wrapper to let us run sync functions on a dedicated thread."""
+    return f(*args, **kwargs)
 
 
 class Honeycomb:
     """Base class to store data locally."""
     _db_thread = asyncio.new_event_loop()
-    _db_connection = _db_thread.run_until_complete(_connect())
-
-    async def __execute(self, query, *args):
-        """Run the given query.
-
-        This coroutine is defined so we can kick execution on to the dedicated
-        thread; subclasses should use the synchronous `_run_query` instead.
-
-        Args:
-            query (string): SQL query to execute.
-
-        Returns:
-            list[sqlite3.Row]: Results of the query.
-        """
-        return self._db_connection.execute(query, *args).fetchall()
+    _db_connection = _db_thread.run_until_complete(
+        _run_async(sqlite3.connect, "hive.db"))
 
     def _run_query(self, query, *args):
         """Run the given query.
@@ -38,4 +25,5 @@ class Honeycomb:
         Returns:
             list[sqlite3.Row]: Results of the query.
         """
-        return self._db_thread.run_until_complete(self.__execute(query, *args))
+        return self._db_thread.run_until_complete(
+            _run_async(self._db_connection.execute, query, *args)).fetchall()
