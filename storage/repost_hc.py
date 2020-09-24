d50224c1a3c9796735202df4abe773664c14973f
Jaysen Spurlock <jayspur11@gmail.com>
2020-09-23 19:30:39 -0700

    Reload what we've written out

--- storage/repost_hc.py
+++ storage/repost_hc.py
@@ -1,3 +1,4 @@
+from workers import repost_worker
 from storage import honeycomb
 
 
@@ -11,33 +12,45 @@ class RepostHC(honeycomb.Honeycomb):
     def _column_names(self):
         return ("GuildID", "ChannelID", "LastMessageID", "URL")
 
-    def create_repost(self, repost_worker):
-        channel = repost_worker.channel
-        message_id = (repost_worker.last_message.id
-                      if repost_worker.last_message else "NULL")
+    def create_repost(self, repost):
+        channel = repost.channel
+        message_id = (repost.last_message.id
+                      if repost.last_message else "NULL")
         self._run_query(
             "INSERT INTO RepostCommands"
             "   VALUES(:guild, :channel, :msg, :url)",
             guild=channel.guild.id,
             channel=channel.id,
             msg=message_id,
-            url=repost_worker.url)
+            url=repost.url)
 
-    def update_repost(self, repost_worker):
-        channel = repost_worker.channel
-        message_id = (repost_worker.last_message.id
-                      if repost_worker.last_message else "NULL")
+    def reload_reposts(self, client):
+        reposts = []
+        for result in self._run_query("SELECT * FROM RepostCommands"):
+            channel = client.get_guild(result["GuildID"]).get_channel(
+                result["ChannelID"])
+            message = client.loop.run_until_complete(
+                channel.fetch_message(result["MessageID"]))
+            url = result["URL"]
+            reposts.append(
+                repost_worker.RepostWorker(channel, url, last_message=message))
+        return reposts
+
+    def update_repost(self, repost):
+        channel = repost.channel
+        message_id = (repost.last_message.id
+                      if repost.last_message else "NULL")
         self._run_query(
             "UPDATE RepostCommands"
             "   SET LastMessageID=:msg, URL=:url"
             "   WHERE GuildID=:guild AND ChannelID=:channel",
             msg=message_id,
-            url=repost_worker.url,
+            url=repost.url,
             guild=channel.guild.id,
             channel=channel.id)
 
-    def delete_repost(self, repost_worker):
-        channel = repost_worker.channnel
+    def delete_repost(self, repost):
+        channel = repost.channnel
         self._run_query(
             "DELETE FROM RepostCommands"
             "   WHERE GuildID=:guild AND ChannelID=:channel",
